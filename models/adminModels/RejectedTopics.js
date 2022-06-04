65c72281213d9b91a2673eafcf240ee0f00d1027
Chanul Gunathilake <87206051+Chanuldg@users.noreply.github.com>
2022-06-04 23:04:12 +0530

    Update the RejectedTopics.js

--- models/adminModels/RejectedTopics.js
+++ models/adminModels/RejectedTopics.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema;//Store the details about rejected topics
 const RejectedTopicSchema = new Schema({
   studentId: {
     type: String,
